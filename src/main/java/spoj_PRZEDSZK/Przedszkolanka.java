231d98570cdb678220909804a86f82e56031473c
Adrian Biernacki <adrianbie86@gmail.com>
2019-08-21 20:32:46 +0200

    SPOJ Przedszkolanaka refactor

--- src/main/java/spoj_PRZEDSZK/Przedszkolanka.java
+++ src/main/java/spoj_PRZEDSZK/Przedszkolanka.java
@@ -7,25 +7,21 @@ public class Przedszkolanka {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt();
-        int mniejsza;
-        int wieksza;
-        int tmp;
-
+        int N = Integer.parseInt(scanner.nextLine());
 
         for (int i = 0; i < N; i++){
 
-            Scanner scanner1 = new Scanner(System.in);
-            String tekst = scanner1.nextLine();
+            String tekst = scanner.nextLine();
 
             String[] tekstTab = tekst.split(" ");
-            mniejsza = Integer.parseInt(tekstTab[0]);
-            wieksza = Integer.parseInt(tekstTab[1]);
+            int mniejsza = Integer.parseInt(tekstTab[0]);
+            int wieksza = Integer.parseInt(tekstTab[1]);
 
+            int tmp;
             if (mniejsza > wieksza){
                 tmp = mniejsza;
-                wieksza = mniejsza;
-                mniejsza = tmp;
+                mniejsza = wieksza;
+                wieksza = tmp;
             }
 
             int wynik = wieksza;
