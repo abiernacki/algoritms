28c5a3ad17085a80d94cdadc03df89b58ed4cb4c
Adrian Biernacki <adrianbie86@gmail.com>
2019-08-26 23:37:02 +0200

    Obżartuchy refactor

--- src/main/java/spoj_GLUTTON/Obzartuchy.java
+++ src/main/java/spoj_GLUTTON/Obzartuchy.java
@@ -1,26 +1,29 @@
 package spoj_GLUTTON;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 
 public class Obzartuchy {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        Scanner scanner = new Scanner(System.in);
-        int n = Integer.parseInt(scanner.nextLine());
+        BufferedReader scanner = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(scanner.readLine());
 
         for (int i = 0; i < n; i++){
 
-            String[] tab = scanner.nextLine().split(" ");
+            String[] tab = scanner.readLine().split(" ");
             int ileOsob = Integer.parseInt(tab[0]);
             double iloscCiastek = Double.parseDouble(tab[1]);
             double day = 86400;
             double zjedli = 0;
             for (int j = 0; j < ileOsob; j++){
 
-                zjedli += Math.floor(day / Double.parseDouble(scanner.nextLine()));
+                zjedli += Math.floor(day / Double.parseDouble(scanner.readLine()));
             }
-            System.out.println(Math.ceil(zjedli / iloscCiastek));
+            System.out.println((int)Math.ceil(zjedli / iloscCiastek));
         }
     }
 }
